7095a4b88c954f9298661dc4727d9032c4ef359a
NAD777 <nehaev07.07.2003@gmail.com>
2019-02-06 18:09:32 +0300

    add functions to conversions of images

---
+++ binimg.py
@@ -0,0 +1,34 @@
+import sys
+import sqlite3 as lite
+
+
+# Функция открытия изображения в бинарном режиме
+def img_to_bin(filename):
+    try:
+        fin = open(filename, "rb")
+        img = fin.read()
+        return img
+    except IOError:
+        print("Error")
+        sys.exit(1)
+    finally:
+        if fin:
+            fin.close()
+
+
+def bin_to_img(data, name_of_file):
+    try:
+        fout = open(name_of_file, 'wb')
+        fout.write(data)
+
+    except IOError:
+        print('Error while writing')
+        sys.exit(1)
+    finally:
+        if fout:
+            fout.close()
+
+"""
+guide http://python-3.ru/page/sqlite-blob-to-image-example
+http://python-3.ru/page/sqlite-upload-image-in-database
+"""
